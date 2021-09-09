f1b60310fa8ce1872a8b4351323ae591be664629
mark-bell-tna <mark.bell@nationalarchives.gov.uk>
2021-09-09 09:53:11 +0100

    Update ukgwa_url.py
    Changes to url prefix and incorporating mp_ into urls for crawling purposes. Reflects recent changes in the structure of UKGWA pages.

--- Code/ukgwa_url.py
+++ Code/ukgwa_url.py
@@ -7,23 +7,48 @@ class UKGWAurl:
 
         self.url = url
         self.hasher = hash_alg
-        self.ukgwa_prefix = "webarchive.nationalarchives.gov.uk/ukgwa/"
+        # There is a new format of url which is a combination of primary and secondary
+        # Including backward compatibility for urls in old style, i.e. without ukgwa/
+        self.ukgwa_primary_prefix = "webarchive.nationalarchives.gov.uk/"
+        self.ukgwa_secondary_prefix = "ukgwa/"
+        self.ukgwa_prefix = self.ukgwa_primary_prefix + self.ukgwa_secondary_prefix
         self.ukgwa_protocol = "https://"
         self.prefix_len = len(self.ukgwa_prefix)
+        primary_pos = self.url.find(self.ukgwa_primary_prefix)
         prefix_pos = self.url.find(self.ukgwa_prefix)
         if prefix_pos >= 0:
-            next_prefix_pos = self.url.find(self.ukgwa_prefix, prefix_pos+1)
-            if next_prefix_pos > 0:
-                self.url = url[next_prefix_pos + self.prefix_len:]
+            this_prefix_pos = prefix_pos
+            prefix_len = len(self.ukgwa_prefix)
+        elif primary_pos >= 0:
+            this_prefix_pos = primary_pos
+            prefix_len = len(self.ukgwa_primary_prefix)
+        else:
+            this_prefix_pos = -1
+        # Sometimes we end up with two prefixes, this will trim them both off
+        if this_prefix_pos >= 0:
+            next_prefix_pos = self.url.find(self.ukgwa_prefix, this_prefix_pos+1)
+            next_primary_pos = self.url.find(self.ukgwa_primary_prefix, this_prefix_pos+1)
+            if next_prefix_pos >= 0:
+                this_next_pos = next_prefix_pos
+                prefix_len = len(self.ukgwa_prefix)
+            elif next_primary_pos >= 0:
+                this_next_pos = next_primary_pos
+                prefix_len = len(self.ukgwa_primary_prefix)
             else:
-                self.url = url[prefix_pos + self.prefix_len:]
+                this_next_pos = -1
+            if this_next_pos > 0:
+                self.url = url[this_next_pos + prefix_len:]
+            else:
+                self.url = url[this_prefix_pos + prefix_len:]
         if len(self.url) > 0:
             if self.url[0] == "/":
                 self.url = self.url[1:]
             self.snap_len = 14
-            if self.url[:2] in ['20','19']:  # assume snapshot
+            if self.url[:2] in ['20','19']:  # assume snapshot - could do this with a regex
                 slash_pos = self.url.find("/")
                 self.snapshot = self.url[:slash_pos]
+                if self.snapshot[-3:] == "mp_":  # mp_ is needed to crawl content of pages, without it we get a UKGWA wrapper page
+                    self.snapshot = self.snapshot[:-3]
                 self.url = self.url[slash_pos+1:]
             else:
                 self.snapshot = None
@@ -64,7 +89,7 @@ class UKGWAurl:
     def set_redirect(self, redirected_url):
         self.actual_url = UKGWAurl(redirected_url, hash_alg = self.hasher)
 
-    def get_url(self, actual=True, snapshot=True, prefix=True):
+    def get_url(self, actual=True, snapshot=True, crawl=False, prefix=True):
         if actual:
             this_url = self.actual_url
         else:
@@ -73,7 +98,10 @@ class UKGWAurl:
         out_url = this_url.url
         if snapshot:
             if this_url.snapshot is not None:
-                out_url = this_url.snapshot + "/" + out_url
+                if crawl:
+                    out_url = this_url.snapshot + "mp_/" + out_url
+                else:
+                    out_url = this_url.snapshot + "/" + out_url
         if prefix:
             out_url = this_url.ukgwa_protocol + this_url.ukgwa_prefix + out_url
 
@@ -100,6 +128,9 @@ if __name__ == "__main__":
     U2 = UKGWAurl("webarchive.nationalarchives.gov.uk/20150305164200///www.keepbritaintidy.org/fly-tipping/-1/20/1/1005/0/o/0c48dd94-9878-42d1-9c01-64f474ae2230")
     U2 = UKGWAurl("//webarchive.nationalarchives.gov.uk/20150305163947///www.keepbritaintidy.org/fly-tipping/-1/20/1/1005/0/o/0c48dd94-9878-42d1-9c01-64f474ae2230")
     U2 = UKGWAurl("https://webarchive.nationalarchives.gov.uk/20150305164356/http://www.keepbritaintidy.org/webarchive.nationalarchives.gov.uk/20150305164348///www.keepbritaintidy.org/fly-tipping/-1/20/1/1005/0/o/0c48dd94-9878-42d1-9c01-64f474ae2230")
+    parent_url = "https://webarchive.nationalarchives.gov.uk/20090101000000/http://www.salt.gov.uk"
+    U = UKGWAurl(parent_url)
+    U2 = UKGWAurl("about_this_site.html",U)
     print(U2.get_domain())
     print(U2.get_snapshot())
-    print(U2)
+    print(U2.get_url(crawl=True))
